e82c9afd348fb20d23d487c5ff5ac75bc27920e8
gh1042143546 <1042143546@qq.com>
2019-03-04 23:48:17 +0800

    test

--- learnDjango/booktest/urls.py
+++ learnDjango/booktest/urls.py
@@ -5,5 +5,6 @@ from django.conf.urls import  url
 from booktest import views
 
 urlpatterns=[
-    url(r'^$',views.index)
+    url(r'^$',views.index),
+    url(r'^(\d+)$',views.show) #提取出来的值，被 views.py 中的对应视图函数接收
 ]
--- learnDjango/booktest/views.py
+++ learnDjango/booktest/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import *
 from django.template import RequestContext,loader
+from .models import *
 # Create your views here.
+
 def index(request):#第一个参数是request
     #temp = loader.get_template('booktest/index.html')#加载模板,返回一个模板对象
     #return HttpResponse(temp.render())#render()渲染模板
-    return render(request,'booktest/index.html')#渲染模板,加载
+    booklist = BookInfo.objects.all()
+    context = {'list':booklist}
+    return render(request,'booktest/index.html',context)#渲染模板,加载
+def show(request,id):
+    book = BookInfo.objects.get(pk=id)
+    herelist = book.heroinfo_set.all()
+    context = {'list':herelist}
+    return render(request,'booktest/show.html',context)
